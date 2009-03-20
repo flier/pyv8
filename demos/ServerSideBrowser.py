8868b79ebc05695fe8dee89f45a17cb809b5a31b
Flier Lu <flier.lu@gmail.com>
2009-03-20 11:17:41 +0000

    begin to trace objects in web session

--- demos/ServerSideBrowser.py
+++ demos/ServerSideBrowser.py
@@ -164,12 +164,47 @@ class HtmlLocation(PyV8.JSClass):
 class HtmlWindow(PyV8.JSClass):
     timers = []
     
+    class Object(PyV8.JSClass):
+        def __init__(self, name):            
+            self.name = name
+            
+        def __getattribute__(self, name):
+            try:
+                return object.__getattribute__(self, name)
+            except:
+                pass
+            
+            me = object.__getattribute__(self, "name")
+            
+            return HtmlWindow.Object("%s.%s" % (me, name))
+            
+        def __setattr__(self, name, value):
+            object.__setattr__(self, name, value)
+        
+        def __str__(self):
+            logging.info("get %s", self.name)
+            
+            return ""
+        
+        def __repr__(self):
+            return "<Object %s at 0x%08X>" % (self.name, id(self))
+                
     def __init__(self, page, dom):
         self.page = page
         self.doc = w3c.getDOMImplementation(dom)
         self.doc.location = self.location
         self.ctxt = PyV8.JSContext(self)
+    """
+    def __getattribute__(self, name):
+        try:
+            return object.__getattribute__(self, name)
+        except AttributeError:
+            pass
+                
+        logging.warn("missing global variable: %s", name)
         
+        return HtmlWindow.Object(name)
+    """
     @property
     def window(self):
         return self
@@ -189,6 +224,13 @@ class HtmlWindow(PyV8.JSClass):
         pass
     
     location = property(getLocation, setLocation)
+    
+    def Image(self):
+        return self.doc.createElement('img')
+    """
+    def ActiveXObject(self, name, server=None):
+        return HtmlWindow.Object(name)
+    """
     
     def setTimeout(self, code, interval, lang="JavaScript"):
         self.timers.append((interval, code))
@@ -247,6 +289,8 @@ class FetchFile(Task):
             return urllib2.urlopen(self.url)
         except:
             logging.warn("fail to fetch %s: %s", self.url, traceback.format_exc())
+            
+            return None
 
 class Evaluator(Task):
     def __init__(self, target):
@@ -259,15 +303,50 @@ class Evaluator(Task):
             self.target.eval(self.pipeline)
         except:
             logging.warn("fail to evalute %s: %s", self.target, traceback.format_exc())
-        
+            
         return self.target
     
     def __repr__(self):
         return "<Evaluator object for %s at 0x%08X>" % (self.target, id(self))
         
-class WebScript(object):
-    def __init__(self, page, value):
-        self.page = page
+class WebObject(object):
+    context = []
+    
+    def __enter__(self):
+        self.context.append(self)
+        
+        logging.debug("entering %s...", self)
+        
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.context.pop()
+        
+        logging.debug("leaving %s...", self)
+    
+    def __init__(self, parent, url):
+        self.children = []
+        self.parent = parent
+        self.url = url
+        
+    @staticmethod
+    def current():        
+        current = WebObject.context[-1] if len(WebObject.context) > 0 else None
+                
+        return current
+        
+    @property
+    def page(self):        
+        tag = self.parent
+        
+        while not isinstance(tag, WebPage):
+            tag = tag.parent
+            
+        return tag
+    
+class WebScript(WebObject):
+    def __init__(self, parent, value, url):
+        WebObject.__init__(self, parent, url)
         
         if type(value) in [str, unicode]:
             self.script = value
@@ -276,29 +355,33 @@ class WebScript(object):
         else:
             self.func = value
         
-    def eval(self, pipeline):       
-        if hasattr(self, "script"):
-            result = self.page.window.eval(self.script)
-        else:
-            result = self.page.window.execute(self.func)
+    def eval(self, pipeline):
+        if len(WebObject.context) > 0:
+            WebObject.context[-1].children.append((None, self))
         
-class WebCss(object):
-    def __init__(self, page, value):
-        self.page = page
+        with self:
+            if hasattr(self, "script"):
+                self.result = self.page.window.eval(self.script)
+            else:
+                self.result = self.page.window.execute(self.func)
+   
+class WebCss(WebObject):
+    def __init__(self, parent, value, url):
+        WebObject.__init__(self, parent, url)
         
         self.css = value if type(value) in [str, unicode] else value.read()
         
     def eval(self, pipeline):
         logging.info("evalute css: %s...", self.css[:20])
         
-class WebPage(object):
-    children = []
-    
-    def __init__(self, parent, response):
-        self.parent = parent
+        with self:
+            pass
+        
+class WebPage(WebObject):
+    def __init__(self, parent, response, url):
+        WebObject.__init__(self, parent, url)
         
         self.code = response.code
-        self.url = response.geturl()
         self.headers = response.headers
 
         html = response.read()
@@ -311,48 +394,52 @@ class WebPage(object):
     def __repr__(self):
         return "<WebPage at %s>" % self.url
     
-    def evalScript(self, pipeline, script):
+    def evalScript(self, pipeline, script, parent):        
         if script.has_key("type") and script["type"] != "text/javascript":
             raise NotImplementedError("not support script type %s", script["type"])
         elif script.has_key("src"):
+            if script["src"].startswith("http://www.google-analytics.com"):
+                return None
+            
             return pipeline.openScript(self, script["src"],
-                lambda child: self.children.append((script, child)))
+                lambda child: parent.children.append((script, child)))
         else:
             return pipeline.evalScript(self, unicode(script.string).encode("utf-8"),
-                lambda child: self.children.append((script, child)))
+                lambda child: parent.children.append((script, child)))
                     
-    def evalTag(self, pipeline, tag):
-        tasks = []            
-        
-        for iframe in tag.findAll('iframe'):
-            tasks.append(pipeline.openPage(self, iframe["src"],
-                lambda page: self.children.append((iframe, page))))
-        
-        for frame in tag.findAll('frame'):
-            tasks.append(pipeline.openPage(self, frame["src"],
-                lambda page: self.children.append((frame, page))))
-        
-        for link in tag.findAll('link', rel='stylesheet', type='text/css', href=True):
-            tasks.append(pipeline.openCss(self, link["href"],
-                lambda css: self.children.append((link, css))))
+    def evalTag(self, pipeline, tag, parent):        
+        with parent:
+            tasks = []            
             
-        for style in tag.findAll('style,', type='text/css'):
-            tasks.append(pipeline.evalCss(self, unicode(style.string).encode("utf-8"),
-                lambda css: self.children.append((link, css))))
-
-        for script in tag.findAll('script'):
-            tasks.append(self.evalScript(pipeline, script))
-
+            for iframe in tag.findAll('iframe'):
+                tasks.append(pipeline.openPage(self, iframe["src"],
+                    lambda page: parent.children.append((iframe, page))))
+            
+            for frame in tag.findAll('frame'):
+                tasks.append(pipeline.openPage(self, frame["src"],
+                    lambda page: parent.children.append((frame, page))))
+            
+            for link in tag.findAll('link', rel='stylesheet', type='text/css', href=True):
+                tasks.append(pipeline.openCss(self, link["href"],
+                    lambda css: parent.children.append((link, css))))
+                
+            for style in tag.findAll('style,', type='text/css'):
+                tasks.append(pipeline.evalCss(self, unicode(style.string).encode("utf-8"),
+                    lambda css: parent.children.append((link, css))))
+    
+            for script in tag.findAll('script'):
+                tasks.append(self.evalScript(pipeline, script, parent))
+    
         return tasks
             
     def eval(self, pipeline):
         with self.window.ctxt:
             scripts = []
                 
-            self.window.document.onCreateElement = lambda element: scripts.append(element) if element.tagName == "script" else None
-            self.window.document.onDocumentWrite = lambda element: self.evalTag(pipeline, element.tag)
+            self.window.document.onCreateElement = lambda element: scripts.append((element, WebObject.current())) if element.tagName == "script" else None
+            self.window.document.onDocumentWrite = lambda element: self.evalTag(pipeline, element.tag, WebObject.current())
             
-            tasks = self.evalTag(pipeline, self.dom)            
+            tasks = self.evalTag(pipeline, self.dom, self)
                     
             Task.waitAll(tasks)
             
@@ -361,28 +448,61 @@ class WebPage(object):
             for interval, code in self.window.timers:
                 tasks.append(pipeline.evalScript(self, code))
                 
-            for script in scripts:
-                tasks.append(self.evalScript(pipeline, script.tag))
-            
-            body = self.dom.find('body')
-            
-            if body and body.has_key('onload'):
-                script = body['onload']
+            try:
+                scripts.append((self.window.document.body['onload'], self))
+            except:
+                pass
                 
-                if script.startswith("vbscript:"):
-                    raise NotImplementedError("not support script type %s", script["type"])
-                elif script.startswith("javascript:"):
-                    script = script[len("javascript:"):]
-                
-                tasks.append(pipeline.evalScript(self, script,
-                    lambda child: self.children.append((body, child))))
-
+            for script, parent in scripts:
+                with parent:
+                    tasks.append(self.evalScript(pipeline, script.tag, parent))
+            
 class WebSession(object):
     def __init__(self, root):
         self.root = root
         
     def __repr__(self):
-        return "<WebSession at %s>" % self.root.url        
+        return "<WebSession at %s>" % self.root.url
+    
+    def dumpName(self, obj):
+        if isinstance(obj, WebCss): return "Css%d" % id(obj)
+        if isinstance(obj, WebScript): return "Script%d" % id(obj)
+        if isinstance(obj, WebPage): return "Page%d" % id(obj)
+        return "Object%d" % id(obj)
+    
+    def dumpChildren(self, out, obj):
+        for tag, child in obj.children:
+            if isinstance(child, WebCss):
+                self.dumpCss(out, child)
+            elif isinstance(child, WebScript):
+                self.dumpScript(out, child)
+            elif isinstance(child, WebPage):
+                self.dumpPAge(out, child)
+    
+    def dumpCss(self, out, css):
+        print >>out, '%s [label="%s"];' % (self.dumpName(css), css.url or "inline CSS")
+        print >>out, '%s -> %s;' % (self.dumpName(css.parent), self.dumpName(css))
+        
+        self.dumpChildren(out, css)
+        
+    def dumpScript(self, out, script):
+        print >>out, '%s [label="%s"];' % (self.dumpName(script), script.url or "inline Script")
+        print >>out, '%s -> %s;' % (self.dumpName(script.parent), self.dumpName(script))
+        
+        self.dumpChildren(out, script)
+    
+    def dumpPage(self, out, page):
+        print >>out, '%s [label="%s"];' % (self.dumpName(page), page.url)
+        
+        self.dumpChildren(out, page)        
+    
+    def save(self, filename):
+        with open(filename, "w") as f:
+            print >>f, "digraph WebSession {"
+            
+            self.dumpPage(f, self.root)
+            
+            print >>f, "}"
         
 class Pipeline(object):
     def __init__(self):
@@ -395,8 +515,12 @@ class Pipeline(object):
     
     def queue(self, task, callback):
         try:
-            task.pipeline = self            
-            task.result = callback(task())
+            task.pipeline = self
+            
+            result = task()
+            
+            if result:
+                task.result = callback(result)
             
             return task
         except:
@@ -409,7 +533,7 @@ class Pipeline(object):
         
     def getEvaluator(self, clazz):
         def evaluator(parent, target, callback=None):
-            self.queue(Evaluator(clazz(parent, target)), 
+            self.queue(Evaluator(clazz(parent, target, None)), 
                 lambda result: callback(result) if callback else None)
             
         return evaluator
@@ -420,7 +544,7 @@ class Pipeline(object):
                 url = urlparse.urljoin(parent.url, url)
             
             self.queue(FetchFile(url),
-                lambda response: self.queue(Evaluator(clazz(parent, response)),
+                lambda response: self.queue(Evaluator(clazz(parent, response, url)),
                     lambda result: callback(result) if callback else None))
             
         return opener
@@ -525,7 +649,7 @@ class Browser(object):
             "names" : ["sessions", "s"],
             "help" : "list the web sessions",
             "handler" : lambda self, line: self.listSessions(line)
-        }
+        },
     )
     
     def runCommand(self, line):
@@ -581,7 +705,7 @@ class Browser(object):
                 
         self.mode = "python"
                 
-        self.console = code.InteractiveConsole({"sessions" : []})
+        self.console = code.InteractiveConsole({"sessions" : self.sessions})
         
         self.terminated = False
         
