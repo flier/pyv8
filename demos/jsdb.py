41ae82c6a3ade525888532a6af28242b780900db
Flier Lu <flier.lu@gmail.com>
2011-03-12 16:45:30 +0000

    jsdb implement shell and OS class

--- demos/jsdb.py
+++ demos/jsdb.py
@@ -22,47 +22,22 @@ class cmd(object):
 
         return wrapped
 
-class Debugger(PyV8.JSDebug, threading.Thread):
+class Debugger(PyV8.JSDebugger, threading.Thread):
     log = logging.getLogger("dbg")
 
-    def __init__(self, ctxt, *args, **kwargs):
-        PyV8.JSDebug.__init__(self)
+    def __init__(self):
+        PyV8.JSDebugger.__init__(self)
         threading.Thread.__init__(self, name='dbg')
 
         self.terminated = False
-        self.ctxt = ctxt
         self.daemon = True
-
-        self.args = args
-        self.kwargs = kwargs
-
-        self.cmds = []
-        self.alias = {}
-
-        self.buildCmds()
 
     def showCopyright(self):
         print "jsdb with PyV8 v%s base on Google v8 engine v%s" % (PyV8.__version__, PyV8.JSEngine.version)
         print "Apache License 2.0 <http://www.apache.org/licenses/LICENSE-2.0>"
 
-    def __enter__(self):
-        self.enabled = True
-
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        PyV8.debugger.enabled = False
-
     def onMessage(self, msg):
         self.log.info("Debug message: %s", msg)
-
-        try:
-            if msg['type'] == 'event' and msg['event'] == 'break':
-                print self.stepNext()
-        except:
-            import traceback
-
-            traceback.print_exc()
 
     def onBreak(self, evt):
         self.log.info("Break event: %s", evt)
@@ -79,47 +54,65 @@ class Debugger(PyV8.JSDebug, threading.Thread):
     def onAfterCompile(self, evt):
         self.log.info("After compile event: %s", evt)
 
-    def run(self):
-        for arg in self.args:
-            if os.path.isfile(arg):
-                with open(arg, 'r') as f:
-                    logging.info("loading script from %s...", arg)
-
-                    ctxt.eval(f.read())
-            else:
-                ctxt.eval(arg)
-
     def shell(self):
         while not self.terminated:
             line = raw_input('(jsdb) ').strip()
 
             if line == '': continue
 
-            args = line.split(' ')
-            cmd = args[0]
-            args = args[1:]
+            try:
+                result = PyV8.JSContext.current.eval(line)
 
-            func = self.alias.get(cmd)
+                if result:
+                    print result
+            except KeyboardInterrupt:
+                break
+            except Exception, e:
+                print e
 
-            if func is None:
-                print "Undefined command: '%s'.  Try 'help'." % cmd
-            else:
-                try:
-                    func(*args)
-                except Exception, e:
-                    print e
+    def debug(self, line):
+        args = line.split(' ')
+        cmd = args[0]
+        args = args[1:]
+
+        func = self.alias.get(cmd)
+
+        if func is None:
+            print "Undefined command: '%s'.  Try 'help'." % cmd
+        else:
+            try:
+                func(*args)
+            except Exception, e:
+                print e
+
+    @property
+    def cmds(self):
+        if not hasattr(self, '_cmds'):
+            self.buildCmds()
+
+        return self._cmds
+
+    @property
+    def alias(self):
+        if not hasattr(self, "_alias"):
+            self.buildCmds()
+
+        return self._alias
 
     def buildCmds(self):
+        self._cmds = []
+        self._alias = {}
+
         for name in dir(self):
             attr = getattr(self, name)
 
             if callable(attr) and hasattr(attr, 'alias'):
-                self.cmds.append(attr)
+                self._cmds.append(attr)
                 
                 for name in attr.alias:
-                    self.alias[name] = attr
+                    self._alias[name] = attr
                     
-        self.log.info("found %d commands with %d alias", len(self.cmds), len(self.alias))
+        self.log.info("found %d commands with %d alias", len(self._cmds), len(self._alias))
 
     @cmd('h', '?')
     def help(self, cmd=None, *args):
@@ -144,9 +137,56 @@ class Debugger(PyV8.JSDebug, threading.Thread):
         """Exit jsdb."""
         self.terminated = True
 
+class OS(PyV8.JSClass):
+    def system(self, *args):
+        return os.system(' '.join(args))
+
+    def chdir(self, path):
+        os.chdir(path)
+
+    def mkdirp(self, path):
+        os.makedirs(path)
+
+    def rmdir(self, path):
+        os.rmdir(path)
+
+    def getenv(self, name):
+        return os.getenv(name)
+
+    def setenv(self, name, value):
+        os.putenv(name, value)
+
+    def unsetenv(self, name):
+        os.unsetenv(name)
+
+    def umask(self, mask):
+        return os.umask(mask)
+
 class Shell(PyV8.JSClass):
-    def alert(self, txt):
-        print txt
+    os = OS()
+    
+    def quit(self, code=0):
+        sys.exit(code)
+
+    def writeln(self, *args):
+        self.write(*args)
+        print
+
+    def write(self, *args):
+        print ' '.join(args),
+
+    def read(self, filename):
+        return open(filename, 'r').read()
+
+    def readline(self):
+        return raw_input()
+
+    def load(self, *filenames):
+        for filename in filenames:
+            PyV8.JSContext.current.eval(open(filename).read())
+
+    def version(self):
+        print "PyV8 v%s with Google V8 v%s" % (PyV8.__version__, PyV8.JSEngine.version)
 
 def parse_cmdline():
     from optparse import OptionParser
@@ -175,11 +215,10 @@ if __name__=='__main__':
     opts, args = parse_cmdline()
 
     with PyV8.JSContext(Shell()) as ctxt:
-        with Debugger(ctxt, *args) as dbg:
+        with Debugger() as dbg:
             dbg.showCopyright()
-            dbg.breakForDebug()
 
-            dbg.start()
+            #dbg.start()
 
             try:
                 dbg.shell()
